a7aa0ef96fe92882d553f8085df3cfd654b1ae98
apsolute.xz <apsolute.xz@gmail.com>
2026-07-02 01:03:34 +0700

    fix-db

--- api/config.php
+++ api/config.php
@@ -66,7 +66,7 @@ function getDB()
     static $pdo = null;
     if ($pdo === null) {
         try {
-            $dsn = 'pgsql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME;
+            $dsn = 'pgsql:host=' . DB_HOST . ';dbname=' . DB_NAME;
 
             $pdo = new PDO(
                 $dsn,
